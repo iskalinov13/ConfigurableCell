4040a722d69e87e71354ac55f5cfdd52dae51e8b
“iskalinov13” <iskalinov133@gmail.com>
2020-08-25 15:06:52 +0600

    Initial Commit

---
+++ ConfigurableCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConfigurableCell
+//
+//  Created by Farukh Iskalinov on 25.08.20.
+//  Copyright © 2020 Farukh Iskalinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
